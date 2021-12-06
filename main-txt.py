52ecde71d54e6b4e60e047a0791ffc82d704910d
”DashaFriday” <watch21angel@gmail.com>
2021-12-06 18:46:52 +0300

    Added comments in txt-ver

--- main-txt.py
+++ main-txt.py
@@ -1,20 +1,28 @@
 import re
 
+# Объявление имён файлов для работы
 source_file = input('Введите файл для проверки: ')
 result_file = 'result.txt'
 
+# Отрываем файл для записи
 with open(result_file, 'w') as out_file:
 
     try:
+
+        # Открываем файл для чтения
         with open(source_file) as in_file:
 
+            # Задаём параметры поиска
             string_for_search = input('Введите строку для поиска: ')
             pattern = re.compile(re.escape(string_for_search))
 
+            # Проходим по каждой строке файла
             for line in in_file:
 
+                # Ищем совпадения
                 result = pattern.search(line)
 
+                # Если совпадение есть, то записываем строку в файл для записи
                 if result is not None:
                     out_file.write(line)
 
